e79da6d3047a4aeb6ea0a8b3d9463af7bb79a88d
Anna Mikhaylova <annamispb@gmail.ccom>
2025-08-30 11:21:52 +0300

    first-version

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,50 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    if (before) {
+        before.reverse().forEach(subName => {
+            const cloned = cloneTemplate(subName);
+            root[subName] = cloned; 
+            root.container.prepend(root[subName].container);
+        });
+    }
+
+    if (after) {
+        after.forEach(subName => {
+            const cloned = cloneTemplate(subName);
+            root[subName] = cloned; 
+            root.container.append(root[subName].container);
+        });
+    }
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    // Обработка события change
+    root.container.addEventListener('change', () => {
+        onAction(); // Вызываем без аргументов
+    });
+
+    // Обработка события reset
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction()); // Отложенный вызов
+    });
+
+    // Обработка события submit
+    root.container.addEventListener('submit', event => {
+        event.preventDefault(); // Предотвращаем стандартное поведение
+        onAction(event.submitter); // Передаем элемент, инициировавший отправку
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => { 
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row.container;
+         })
         root.elements.rows.replaceChildren(...nextRows);
     }
 
