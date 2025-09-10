3f1d4b7ac7ed28fe5bc28d42626fbbc5a8dc2702
Anna Mikhaylova <annamispb@gmail.ccom>
2025-09-10 23:15:31 +0300

    formatted with prettier

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,56 +8,56 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
-    if (before) {
-        before.reverse().forEach(subName => {
-            const cloned = cloneTemplate(subName);
-            root[subName] = cloned; 
-            root.container.prepend(root[subName].container);
-        });
-    }
-
-    if (after) {
-        after.forEach(subName => {
-            const cloned = cloneTemplate(subName);
-            root[subName] = cloned; 
-            root.container.append(root[subName].container);
-        });
-    }
-
-    // @todo: #1.3 —  обработать события и вызвать onAction()
-    // Обработка события change
-    root.container.addEventListener('change', () => {
-        onAction(); // Вызываем без аргументов
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  if (before) {
+    before.reverse().forEach((subName) => {
+      const cloned = cloneTemplate(subName);
+      root[subName] = cloned;
+      root.container.prepend(root[subName].container);
     });
+  }
 
-    // Обработка события reset
-    root.container.addEventListener('reset', () => {
-        setTimeout(onAction()); // Отложенный вызов
+  if (after) {
+    after.forEach((subName) => {
+      const cloned = cloneTemplate(subName);
+      root[subName] = cloned;
+      root.container.append(root[subName].container);
     });
+  }
 
-    // Обработка события submit
-    root.container.addEventListener('submit', event => {
-        event.preventDefault(); // Предотвращаем стандартное поведение
-        onAction(event.submitter); // Передаем элемент, инициировавший отправку
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+  // Обработка события change
+  root.container.addEventListener("change", () => {
+    onAction(); // Вызываем без аргументов
+  });
+
+  // Обработка события reset
+  root.container.addEventListener("reset", () => {
+    setTimeout(onAction()); // Отложенный вызов
+  });
+
+  // Обработка события submit
+  root.container.addEventListener("submit", (event) => {
+    event.preventDefault(); // Предотвращаем стандартное поведение
+    onAction(event.submitter); // Передаем элемент, инициировавший отправку
+  });
+
+  const render = (data) => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+    const nextRows = data.map((item) => {
+      const row = cloneTemplate(rowTemplate);
+      Object.keys(item).forEach((key) => {
+        if (key in row.elements) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+      return row.container;
     });
+    root.elements.rows.replaceChildren(...nextRows);
+  };
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = data.map(item => {
-            const row = cloneTemplate(rowTemplate);
-            Object.keys(item).forEach(key => { 
-                if (key in row.elements) {
-                    row.elements[key].textContent = item[key];
-                }
-            });
-            return row.container;
-         })
-        root.elements.rows.replaceChildren(...nextRows);
-    }
-
-    return {...root, render};
+  return { ...root, render };
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initTable} from "./components/table.js";
-import {initPagination} from "./components/pagination.js";
-import {initSorting} from "./components/sorting.js";
-import {initFiltering} from "./components/filtering.js";
-import {initSearching} from "./components/searching.js";
+import { initTable } from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 
 // Исходные данные используемые в render()
 const api = initData(sourceData);
@@ -20,14 +20,14 @@ const api = initData(sourceData);
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
-    const rowsPerPage = parseInt(state.rowsPerPage);    // приведём количество страниц к числу
-    const page = parseInt(state.page ?? 1);                // номер страницы по умолчанию 1 и тоже число    
-    return {
-        ...state,
-        rowsPerPage,
-        page
-    };
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage); // приведём количество страниц к числу
+  const page = parseInt(state.page ?? 1); // номер страницы по умолчанию 1 и тоже число
+  return {
+    ...state,
+    rowsPerPage,
+    page,
+  };
 }
 
 /**
@@ -35,52 +35,59 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 async function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let query = {}; // копируем для последующего изменения
-    // использование
-    query = applySearching(query, state, action); // result заменяем на query
-    query = applyFiltering(query, state, action); // result заменяем на query
-    query = applySorting(query, state, action); // result заменяем на query
-    query = applyPagination(query, state, action); // обновляем query
-    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
-    updatePagination(total, query); // перерисовываем пагинатор
-    sampleTable.render(items);
+  let state = collectState(); // состояние полей из таблицы
+  let query = {}; // копируем для последующего изменения
+  // использование
+  query = applySearching(query, state, action); // result заменяем на query
+  query = applyFiltering(query, state, action); // result заменяем на query
+  query = applySorting(query, state, action); // result заменяем на query
+  query = applyPagination(query, state, action); // обновляем query
+  const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
+  updatePagination(total, query); // перерисовываем пагинатор
+  sampleTable.render(items);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: ['search', 'header', 'filter'],
-    after: ['pagination']
-}, render);
-
-// инициализация
-const {applyPagination, updatePagination} = initPagination(
-    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
-    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
-        const input = el.querySelector('input');
-        const label = el.querySelector('span');
-        input.value = page;
-        input.checked = isCurrent;
-        label.textContent = page;
-        return el;
-    }
+const sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render
 );
 
-const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
-    sampleTable.header.elements.sortByDate,
-    sampleTable.header.elements.sortByTotal
-]); 
-const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements);                        
+// инициализация
+const { applyPagination, updatePagination } = initPagination(
+  sampleTable.pagination.elements, // передаём сюда элементы пагинации, найденные в шаблоне
+  (el, page, isCurrent) => {
+    // и колбэк, чтобы заполнять кнопки страниц данными
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+    return el;
+  }
+);
+
+const applySorting = initSorting([
+  // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal,
+]);
+const { applyFiltering, updateIndexes } = initFiltering(
+  sampleTable.filter.elements
+);
 const applySearching = initSearching(sampleTable.search.elements);
-const appRoot = document.querySelector('#app');
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
 async function init() {
-    const indexes = await api.getIndexes()
-    updateIndexes(sampleTable.filter.elements, {
-        searchBySeller: indexes.sellers
-    });
-};
+  const indexes = await api.getIndexes();
+  updateIndexes(sampleTable.filter.elements, {
+    searchBySeller: indexes.sellers,
+  });
+}
 
 init().then(render);
